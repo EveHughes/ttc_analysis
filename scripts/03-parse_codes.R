85910d8a37a8ac775ac9ae6ca558677882832554
prajogt <113074010+prajogt@users.noreply.github.com>
2024-01-19 21:15:19 -0500

    Initial commit

---
+++ scripts/03-parse_codes.R
@@ -0,0 +1,12 @@
+#### Preamble ####
+# Purpose: Group delay data into similar sets of incidents
+# Author: Timothius Prajogi
+# Date: 23 January 2024
+# Contact: tim.prajogi@mail.utoronto.ca
+# License: MIT
+# Prerequisites: 01-download_data
+
+#### Workspace setup ####
+library(tidyverse)
+library(janitor)
+
