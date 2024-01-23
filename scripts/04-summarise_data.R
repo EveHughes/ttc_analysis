5de2ab9075b834ae7b4014ba8053751e4ce0eaa7
prajogt <113074010+prajogt@users.noreply.github.com>
2024-01-23 04:34:01 -0500

    Add prereq

--- scripts/04-summarise_data.R
+++ scripts/04-summarise_data.R
@@ -4,7 +4,7 @@
 # Date: 23 January 2024
 # Contact: tim.prajogi@mail.utoronto.ca
 # License: MIT
-# Prerequisites: 01-download_data, 02-clean_data
+# Prerequisites: 01-download_data, 02-clean_data, 03-parse_codes
 
 #### Workspace setup ####
 library(tidyverse)
